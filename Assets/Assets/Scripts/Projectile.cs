5ed82689f0f284dfa9f2aefe8b2f237ec1d81850
MikeHayes <mghayes93@gmail.com>
2019-04-30 14:45:25 -0700

    Added in scriptable object for projectiles

---
+++ Assets/Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "New Projectile")]
+public class Projectile : ScriptableObject
+{
+    [SerializeField] GameObject projPrefab;
+    [SerializeField] int projDMG = 1;
+    [SerializeField] int projNum = 1;
+    [SerializeField] float projSpeed = 20f;
+    [SerializeField] float projFiringPeriod = .1f;
+
+    public GameObject GetProjPrefab() { return projPrefab; }
+    public int GetProjDMG() { return projDMG; }
+    public int GetProjNum() { return projNum; }
+    public float GetProjSpeed() { return projSpeed; }
+    public float GetProjFiringPeriod() { return projFiringPeriod; }
+
+
+}
